f1fb92729be5faf9aeae7222d95af79f143e23f7
mihai <freeride_mihai@yahoo.com>
2021-10-12 14:26:59 +0200

    refactored with hooks

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p>app to search Github users</p>
+      <p>Version 1.0</p>
+    </Fragment>
+  );
+};
